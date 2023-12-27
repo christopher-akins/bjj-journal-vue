5f486a6f1888bc1eff559012e48c48094ec8af07
Christopher Akins <chris.akins@aware3.com>
2023-12-27 09:42:56 -0700

    updated main.ts file - failed save

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
-import { createApp } from 'vue'
-import './style.scss'
-import App from './App.vue'
+import { createApp } from 'vue';
 
-createApp(App).mount('#app')
+import App from './App.vue';
+import router from './router';
+
+import './style.scss';
+
+const app = createApp(App)
+
+app.use(router);
+
+app.mount('#app')
