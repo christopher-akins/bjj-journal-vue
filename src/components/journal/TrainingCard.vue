375b61370de12544142c3bcf4f09ae002e34696c
Christopher Akins <chris.akins@aware3.com>
2023-12-28 14:32:21 -0700

    axios and api implementionat - basic

---
+++ src/components/journal/TrainingCard.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="card">
+    <div class="card-content">
+      <div class="content">
+        <div class="date-wrapper">{{ trainingEvent.trainingDate }}</div>
+        <p>{{ trainingEvent.trainingType }}</p>
+        <h3>{{ trainingEvent.partnerName }}</h3>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+
+export default {
+  props: {
+    trainingEvent: {
+      type: Object,
+      required: true,
+    }
+
+    // trainingDate: {
+    //   type: [String, Date],
+    //   default: new Date(),
+    // },
+
+    // trainingType: {
+    //   type: String,
+    //   default: 'Gi'
+    // },
+
+    // partnerName: {
+    //   type: String,
+    //   default: ''
+    // },
+
+    // partnerBeltRank: {
+    //   type: String,
+    //   default: 'unknown',
+    // },
+
+    // technique: {
+    //   type: String,
+    //   default: '',
+    // },
+
+    // rollingPartners: {
+    //   type: Array,
+    //   default: [],
+    // },
+
+    // personalPerformanceRating: {
+    //   type: Number,
+    //   default: 5,
+    // },
+
+    // preTrainingGoals: {
+    //   type: String,
+    //   default: '',
+    // },
+
+    // postTrainingGoals: {
+    //   type: String,
+    //   default: '',
+    // },
+
+    // generalNotes: {
+    //   type: String,
+    //   default: '',
+    // },
+  },
+
+  setup(props) {
+    console.log(props);
+  }
+}
+</script>
--- src/components/views/TrainingJournalView.vue
+++ src/components/views/TrainingJournalView.vue
@@ -1,12 +1,42 @@
 <template>
-  <div>
-    <h1>All Training</h1>
-  </div>
-
+    <main-banner banner-title="Training History" />
+    <div class="container">
+      <training-card
+        v-for="training in trainings"
+        :key="training.id"
+        :training-event="training"
+      />
+    </div>
 </template>
 
-<script setup lang="ts">
+<script lang="ts">
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
 
+import MainBanner from '@/components/global/layout/MainBanner.vue';
+import TrainingCard from '@/components/journal/TrainingCard.vue';
+
+export default {
+  components: {
+    MainBanner,
+    TrainingCard,
+  },
+
+  setup() {
+    const trainings = ref([]);
+
+    onMounted(async () => {
+      const response = await axios.get('https://my-json-server.typicode.com/christopher-akins/bjj-journal-vue/trainings');
+
+      trainings.value = response.data;
+      console.log(response);
+    });
+
+    return {
+      trainings
+    }
+  }
+}
 </script>
 
 <style scoped lang="scss">
