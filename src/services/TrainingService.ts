f0b7eefd410d9ddb9c566246b360094f6334af6e
Christopher Akins <chris.akins@aware3.com>
2024-03-19 22:06:26 -0700

    small ts changes

--- src/services/TrainingService.ts
+++ src/services/TrainingService.ts
@@ -13,15 +13,17 @@ const axiosClient = axios.create({
 export default {
   async fetchAllTrainings(): Promise<Training[]> {
     const response = await axiosClient.get<Training[]>('/trainings');
+    const { data } = response;
 
-    return response.data;
+    return data;
   },
 
   async fetchSingleTraining(id: string): Promise<Training> {
     const endpoint = `/trainings/${id}`;
 
     const response = await axiosClient.get<Training>(endpoint);
+    const { data } = response;
 
-    return response.data;
+    return data;
   },
 };
--- src/stores/TrainingStore.ts
+++ src/stores/TrainingStore.ts
@@ -25,7 +25,6 @@ export const useTrainingStore = defineStore('TrainingStore', {
     fetchAllTrainings() {
       TrainingService.fetchAllTrainings()
         .then((response) => {
-
           this.trainingList = response;
         })
         .catch((error) => {
