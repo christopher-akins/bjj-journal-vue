8cc35e4565e6e634257db82e25c78e5c12224731
Christopher Akins <chris@christopher.com>
2024-03-24 22:53:30 -0700

    new svg logo to work with

--- src/components/global/layout/SidebarNavigation.vue
+++ src/components/global/layout/SidebarNavigation.vue
@@ -1,12 +1,27 @@
 <template>
   <div class="sidebar-container">
-    <router-link to="/" class="navbar-item logo-wrapper">
-      <img src="/images/bjj-journal-logo.svg" alt="BJJ Journal Logo" />
-    </router-link>
-    <h1>sidebar</h1>
+    <div class="logo-wrapper">
+      <router-link to="/" class="navbar-item logo-wrapper">
+        <img src="/images/bjj-journal-logo.svg" alt="BJJ Journal Logo" />
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 
 </script>
+
+<style lang="scss">
+.sidebar-container {
+  width: 300px;
+}
+
+.logo-wrapper {
+  display: flex;
+
+  img {
+    width: 60px;
+  }  
+}
+</style>
