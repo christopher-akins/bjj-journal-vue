655c6571ab30b5e0e620a1bed469525d9910bc78
Christopher Akins <christopher.v.akins@gmail.com>
2024-03-26 15:09:38 -0700

    good stopping point for sidebar styles

--- src/components/global/layout/SidebarNavigation.vue
+++ src/components/global/layout/SidebarNavigation.vue
@@ -1,27 +1,57 @@
 <template>
   <div class="sidebar-container">
-    <div class="logo-wrapper">
-      <router-link to="/" class="navbar-item logo-wrapper">
-        <img src="/images/bjj-journal-logo.svg" alt="BJJ Journal Logo" />
+      <div class="logo-wrapper">
+        <router-link to="/">
+          <img src="/images/bjj-journal-logo.svg" alt="BJJ Journal Logo" />
+        </router-link>
+      </div>
+      <router-link to="training-new" class="navbar-item">
+        <FontAwesomeIcon :icon="faPencil" class="menu-icon" />
+        <span>New Training</span>
       </router-link>
-    </div>
+      <router-link to="training" class="navbar-item">
+        <FontAwesomeIcon :icon="faPersonRunning" class="menu-icon" />
+        <span>Past Training</span>
+      </router-link>
   </div>
 </template>
 
 <script setup lang="ts">
-
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faPencil, faPersonRunning } from '@fortawesome/free-solid-svg-icons';
 </script>
 
-<style lang="scss">
-.sidebar-container {
-  width: 300px;
+<style scoped lang="scss">
+// For .sidebar-container layout specifics see global styles in
+// `src/scss/layout/_main.scss`
+
+.logo-wrapper {
+  padding-bottom: 1rem;
+  border-bottom: 1px solid var(--primary-font-color);
+  margin-bottom: 1rem;
+}
+
+.navbar-item {
+  display: flex;
+  align-items: center;
+  color: var(--primary-font-color);
+  text-decoration: none;
+  text-transform: uppercase;
+  font-weight: 700;
+  line-height: 30px;
+  margin: 1rem 0;
+
+  .menu-icon {
+    height: 24px;
+    width: 24px;
+    padding-right: 0.8rem;
+  }
 }
 
 .logo-wrapper {
-  display: flex;
 
   img {
     width: 60px;
-  }  
+  }
 }
 </style>
