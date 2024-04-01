5e5d005d0cfdfdcfc72fd03d724a7db82a6a5731
Christopher Akins <cakins@ChristophersMBP.attlocal.net>
2024-04-01 12:30:21 -0400

    updating sidebar for DRY

--- src/components/global/layout/SidebarNavigation.vue
+++ src/components/global/layout/SidebarNavigation.vue
@@ -5,20 +5,39 @@
           <img src="/images/bjj-journal-logo.svg" alt="BJJ Journal Logo" />
         </router-link>
       </div>
-      <router-link to="training-new" class="navbar-item">
-        <FontAwesomeIcon :icon="faPencil" class="menu-icon" />
-        <span>New Training</span>
-      </router-link>
-      <router-link to="training" class="navbar-item">
-        <FontAwesomeIcon :icon="faPersonRunning" class="menu-icon" />
-        <span>Past Training</span>
+      <router-link
+        v-for="link in sidebarLinks"
+        :to="link.toName"
+        class="navbar-item"
+        :key="link.toName"
+      >
+        <FontAwesomeIcon :icon=link.icon class="menu-icon" />
+        <span>{{ link.title }}</span>
       </router-link>
   </div>
 </template>
 
 <script setup lang="ts">
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { faPencil, faPersonRunning } from '@fortawesome/free-solid-svg-icons';
+import { faChartPie, faPencil, faPersonRunning } from '@fortawesome/free-solid-svg-icons';
+
+const sidebarLinks = [
+  {
+    toName: 'training-new',
+    icon: faPencil,
+    title: 'New Training',
+  },
+  {
+    toName: 'training',
+    icon: faPersonRunning,
+    title: 'Past Training',
+  },
+  {
+    toName: 'dashboard',
+    icon: faChartPie,
+    title: 'Dashboard',
+  },
+]
 </script>
 
 <style scoped lang="scss">
@@ -39,7 +58,7 @@ import { faPencil, faPersonRunning } from '@fortawesome/free-solid-svg-icons';
   text-transform: uppercase;
   font-weight: 700;
   line-height: 30px;
-  margin: 1rem 0;
+  margin: 1.25rem 0;
 
   .menu-icon {
     height: 24px;
@@ -49,7 +68,6 @@ import { faPencil, faPersonRunning } from '@fortawesome/free-solid-svg-icons';
 }
 
 .logo-wrapper {
-
   img {
     width: 60px;
   }
